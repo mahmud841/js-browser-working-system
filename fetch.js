5ac958fbbb5084c0e8a9303826a203fd3cdea360
AL MAHMUD SURUJ <mahmudsuruj14@gmail.com>
2021-09-04 19:36:44 +0800

    first commit

---
+++ fetch.js
@@ -0,0 +1,24 @@
+console.log(1133);
+console.log(1133);
+// setTimeout(() => console.log("bhfddskfj"),5000);
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json => console.log(json));
+console.log(3133);
+console.log(4133);
+/* for (let i =0; i<1000; i++) {
+    console.log(i);
+} */
+
+
+console.log(34676);
+console.log(45777);
+
+// fetch o asynchronas vabe kaj kore 
+
+//The finally() method returns a Promise. When the promise is settled, i.e either fulfilled or rejected, the specified callback function is executed. This provides a way for code to be run whether the promise was fulfilled successfully or rejected once the Promise has been dealt with.
+
+/* Heap: Dynamic memory allocation
+The heap is a different space for storing data where JavaScript stores objects and functions.
+Unlike the stack, the engine doesn't allocate a fixed amount of memory for these objects. Instead, more space will be allocated as needed.
+Allocating memory this way is also called dynamic memory allocation. */
---
+++ timeout.js
@@ -0,0 +1,19 @@
+function doSomething() {
+    console.log('I coding Javascript');
+}
+console.log('First: ami sobar caite alada hote cai');
+console.log('Second: I am the boss of my own world ');
+// doSomething();
+// setTimeout(doSomething, 5000);
+setTimeout(function (){
+    console.log('I am using VS code 2021');
+}, 5000);
+
+setTimeout( () => {
+    console.log('I am Exploring MDN ');
+},3000)
+console.log('Third: Ami boss hote cai anycost ');
+console.log('Fourth: Ami ak hali milestone ses koresi ');
+// aigula run korle step by step asbe first ,second, third, fourth avabe. aktar por akta. ai system take synchronas bole. serial moto.
+
+//settimeout use korle seta asynchronas vabe kaj korbe . serial moto korbe na .milisecond declare kore diye jeta kom somoy pabe seta age execute hobe 
